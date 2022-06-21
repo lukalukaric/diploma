06aec34d2f5f820d41e13a37aef5cbc280a070c9
Luka Lukarič <73852345+lukalukaric@users.noreply.github.com>
2022-06-21 19:36:30 +0200

    created questions.txt and class file

---
+++ Backend/src/main/java/diploma/lukaric/backend/Question.java
@@ -0,0 +1,14 @@
+package diploma.lukaric.backend;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Question {
+    private String id;
+    private String question;
+    private String answer;
+}
