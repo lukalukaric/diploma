5cad6e90e48a98c6ecec1876d14ed44136f2f332
Luka Lukarič <73852345+lukalukaric@users.noreply.github.com>
2022-06-24 14:14:27 +0200

    created RestController and Service

---
+++ Backend/src/main/java/diploma/lukaric/backend/RestController.java
@@ -0,0 +1,25 @@
+package diploma.lukaric.backend;
+
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping(path = "/api")
+public class RestController {
+    private final Service service;
+
+    public RestController(Service service) {
+        this.service = service;
+    }
+
+    @CrossOrigin("http://localhost:4200")
+    @PostMapping
+    public RestResponse validateSQLStatement(@RequestBody String statement) {
+        service.validateSQLStatement(statement);
+        return new RestResponse(200,"OK");
+    }
+
+    @GetMapping(path = "/question")
+    public Question getRandomQuestion() {
+        return service.getRandomQuestion();
+    }
+}
---
+++ Backend/src/main/java/diploma/lukaric/backend/Service.java
@@ -0,0 +1,35 @@
+package diploma.lukaric.backend;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    public void validateSQLStatement(String statement){
+        System.out.println("dobili smo: " + statement);
+    }
+
+    public Question getRandomQuestion() {
+        List<Question> questions = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/questions.txt");
+            Scanner scanner = new Scanner(fileInputStream);
+            while (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().split("\"");
+                questions.add(new Question(line[1], line[3], line[5]));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: Unable to read file");
+            questions.add(new Question("-1", "Unable to read file", "ERROR"));
+        }
+        int randomNumber = new Random().nextInt(questions.size());
+        System.out.println("Returning random question: " + questions.get(randomNumber));
+        return questions.get(randomNumber);
+    }
+}
